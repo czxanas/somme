ee248214457fa74cd66236f51ad1c73faf7f7e9c
czxanas <anas.frontdev@gmail.com>
2025-05-17 20:03:34 +0100

    Astro's vercel adapter installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    devToolbar: {
-        enabled: false
-    }
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  devToolbar: {
+      enabled: false
+  },
+
+  adapter: vercel()
 });
