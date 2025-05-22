c23d6aa3167e879c20d8dd1d22da3c8e34cb7acf
czxanas <anas.frontdev@gmail.com>
2025-05-22 01:45:23 +0100

    Ready to use

--- src/data/seo.ts
+++ src/data/seo.ts
@@ -1,4 +1,4 @@
-// import metaImage from '@images/meta.png';
+// import metaImage from '@images/meta.png'; // Need to import it from the right path
 
 export const defaultSeo: Seo = {
     title: 'Astro boilerplate',
@@ -12,16 +12,16 @@ export const defaultSeo: Seo = {
             description: 'Boilerplate for Astro'
         },
         twitter: {
-            creator: '@LocomotiveMTL',
-            title: 'Astro boilerplate',
+            creator: 'Anas',
+            title: 'Astro basic boilerplate',
             image: {
                 // url: metaImage.src
             },
-            description: 'Boilerplate for Astro'
+            description: 'Basic boilerplate for Astro'
         }
     },
     advanced: {
         robots: ['noindex', 'nofollow'],
-        // canonical: 'https://locomotive.ca'
+        // canonical: 'https://yourdomain.ca'
     }
 };
